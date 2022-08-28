c76e12825ebd5ad56f6d3d8debfcc9a4e18412f6
heumsi <heumsi@naver.com>
2022-08-28 18:57:14 +0900

    practice: Subnet Network를 추가한다

--- practice/main.tf
+++ practice/main.tf
@@ -16,3 +16,10 @@ resource "google_compute_network" "hotwg_prod_1" {
   name                    = "hotwg-prod-1"
   auto_create_subnetworks = false
 }
+
+resource "google_compute_subnetwork" "hotwg_asne3_prod_1" {
+  name          = "hotwg-asne3-prod-1"
+  ip_cidr_range = "10.1.0.0/16"
+  region        = "asia-northeast3"
+  network       = google_compute_network.hotwg_prod_1.id
+}
