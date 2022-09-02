281f48ccd7ee27fbae99c777dd3d44f789f37525
heumsi <heumsi@naver.com>
2022-09-03 00:40:17 +0900

    practice: variables.tf 에 type과 desc를 추가한다

--- practice/modules/compute/variables.tf
+++ practice/modules/compute/variables.tf
@@ -1,11 +1,15 @@
 variable "ssh_pub_key_file" {
+  description = "gce public key used by ssh file path"
 }
 
 variable "service_account" {
+  description = "terraform google_service_account resource"
 }
 
 variable "subnetwork" {
+  description = "terraform google_compute_subnetwortk resource"
 }
 
 variable "nat_ip" {
+  description = "terraform google_compute_address resource"
 }
--- practice/variables.tf
+++ practice/variables.tf
@@ -1,8 +1,16 @@
 variable "project" {
+  type        = string
+  description = "gcp project id"
+  default     = "storied-channel-359115"
+
 }
 
 variable "credentials_file" {
+  type        = string
+  description = "gcp serviceaccount used by terraform json file path"
 }
 
 variable "gce_ssh_pub_key_file" {
+  type        = string
+  description = "gce public key used by ssh file path "
 }
